c488371e57ca93c34a5fbdcf810615bfa57d0f33
unknown <nandinhaa1280@gmail.com>
2025-03-11 15:21:10 -0300

    adicionou ponto de exclamação na mensagem de boas vindas

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 let nomeUsuario = prompt("Escreva seu nome:"); //variável "nomeUsuario" que armazena o nome do usuário
-console.log ("Seja bem-vindo(a)" +nomeUsuario);
+console.log ("Seja bem-vindo(a)" +nomeUsuario+"!");
